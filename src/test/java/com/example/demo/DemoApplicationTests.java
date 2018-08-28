b8f12a740b9eeef5258561a98a7f576ec3d86526
Rafał Wieteska <deli.gares@gmail.com>
2018-08-28 22:32:27 +0200

    patternMatching

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -8,16 +8,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import io.vavr.Function1;
+import io.vavr.Function2;
+import io.vavr.Function5;
 import io.vavr.Tuple;
+
+import static io.vavr.API.*;
 
 import static org.assertj.core.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
-	
+
 	private VavrService vavrService;
-	
+
 	@Before
 	public void init() {
 		vavrService = new VavrService();
@@ -27,24 +31,49 @@ public class DemoApplicationTests {
 	public void contextLoads() {
 		assertThat(vavrService.test()).isEqualTo(Tuple.of("xx", 2));
 	}
-	
+
 	@Test
 	public void testComposition1() {
-		Function1<Integer, Integer> plusOne = a -> a + 1;
-		Function1<Integer, Integer> multiplyByTwo = a -> a * 2;
+		Function1<String, String> sayHello = name -> "Hello " + name;
+		Function1<String, String> doGreeting = hello -> hello + ", nice to see you!";
 
-		Function1<Integer, Integer> add1AndMultiplyBy2 = plusOne.andThen(multiplyByTwo);
+		Function1<String, String> sayHelloAndGreet = sayHello.andThen(doGreeting);
 
-		assertThat(add1AndMultiplyBy2.apply(2)).isEqualTo(6);
+		assertThat(sayHelloAndGreet.apply("Stefan")).isEqualTo("Hello Stefan, nice to see you!");
 	}
-	
+
 	@Test
 	public void testComposition2() {
+		Function1<String, String> sayHello = name -> "Hello " + name;
+		Function1<String, String> doGreeting = hello -> hello + ", nice to see you!";
+
+		Function1<String, String> sayHelloAndGreet = sayHello.andThen(doGreeting);
+
+		assertThat(sayHelloAndGreet.apply("Stefan")).isEqualTo("Hello Stefan, nice to see you!");
+	}
+
+	@Test
+	public void testPartialApplication1() {
 		Function1<Integer, Integer> plusOne = a -> a + 1;
 		Function1<Integer, Integer> multiplyByTwo = a -> a * 2;
 
 		Function1<Integer, Integer> add1AndMultiplyBy2 = multiplyByTwo.compose(plusOne);
 
 		assertThat(add1AndMultiplyBy2.apply(2)).isEqualTo(6);
+	}
+
+	@Test
+	public void testPartial2() {
+		Function5<Integer, Integer, Integer, Integer, Integer, Integer> sum = (a, b, c, d, e) -> a + b + c + d + e;
+		Function2<Integer, Integer, Integer> add6 = sum.apply(2, 3, 1);
+
+		assertThat(add6.apply(4, 3)).isEqualTo(13);
+	}
+
+	@Test
+	public void testPatternMatching1() {
+		Integer req = 1;
+		String s = Match(req).of(Case($(1), "one"), Case($(2), "two"), Case($(), "?"));
+		System.out.println(s);
 	}
 }
